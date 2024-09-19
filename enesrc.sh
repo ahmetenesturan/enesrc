3ba1a2887baaf1a0010aac4edcf6d594efe15636
Ahmet Enes Turan <ahmetenesturan@gmail.com>
2024-09-19 21:43:59 +0300

    first commit

---
+++ enesrc.sh
@@ -0,0 +1,3 @@
+alias start_iictools="bash ~/projects/ic_tools/iic-osic-tools/start_x.sh"
+alias start_openfasoc="xhost +Local:* && docker container start glayoutcontainer"
+alias stop_openfasoc="docker container stop glayoutcontainer"
