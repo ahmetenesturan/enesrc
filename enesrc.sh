2aed6677a601ac493985e7ffb587b49f897a2613
Ahmet Enes Turan <ahmetenesturan@gmail.com>
2024-09-19 21:58:28 +0300

    added line

--- enesrc.sh
+++ enesrc.sh
@@ -1,3 +1,5 @@
 alias start_iictools="bash ~/projects/ic_tools/iic-osic-tools/start_x.sh"
 alias start_openfasoc="xhost +Local:* && docker container start glayoutcontainer"
 alias stop_openfasoc="docker container stop glayoutcontainer"
+
+shopt -s cdspell
