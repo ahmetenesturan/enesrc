951598aa1ed9b7e648daaf4bb2c10739c5fc4872
Ahmet Enes Turan <ahmetenesturan@gmail.com>
2025-01-12 11:00:34 +0300

    added matlab alias

--- enesrc.sh
+++ enesrc.sh
@@ -5,5 +5,6 @@ alias updateall="sudo dnf update -y && flatpak update -y"
 alias dnfinstall="sudo dnf install"
 alias speedtest="speedtest-rs"
 alias quickcommit="git add . && git commit -m \"quick commit\" && git push"
+alias matlab="env LD_PRELOAD=/usr/lib64/libstdc++.so.6 GTK_PATH=/usr/lib64/gtk-3.0 matlab -desktop ."
 
 shopt -s cdspell
