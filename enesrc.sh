4eb2376b625564be5bb8f07f94569fd43b20f00b
Ahmet Enes Turan <ahmetenesturan@gmail.com>
2025-02-09 20:09:41 +0300

    changed iic-osic-tools alias

--- enesrc.sh
+++ enesrc.sh
@@ -1,4 +1,4 @@
-alias start_iictools="bash ~/projects/IC_TOOLS/iic-osic-tools/start_x.sh"
+alias iictools="bash ~/projects/IC_TOOLS/iic-osic-tools/start_x.sh"
 alias start_openfasoc="xhost +Local:* && docker container start glayoutcontainer"
 alias stop_openfasoc="docker container stop glayoutcontainer"
 alias updateall="sudo dnf update -y && flatpak update -y"
