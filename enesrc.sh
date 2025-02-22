facb0096190667beafa4ada285776b8ee736eb68
Ahmet Enes Turan <ahmetenesturan@gmail.com>
2025-02-22 18:14:49 +0300

    added snap refresh to updateall command

--- enesrc.sh
+++ enesrc.sh
@@ -1,7 +1,7 @@
 alias iictools="bash ~/projects/IC_TOOLS/iic-osic-tools/start_x.sh"
 alias start_openfasoc="xhost +Local:* && docker container start glayoutcontainer"
 alias stop_openfasoc="docker container stop glayoutcontainer"
-alias updateall="sudo dnf update -y && flatpak update -y"
+alias updateall="sudo dnf update -y && flatpak update -y && sudo snap refresh -y"
 alias dnfinstall="sudo dnf install"
 alias speedtest="speedtest-rs"
 alias quickcommit="git add . && git commit -m \"quick commit\" && git push"
